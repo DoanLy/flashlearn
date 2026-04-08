edfc0f2069c3485cad0555c05038f585d91b4257
DoanLy <doanly1996@gmail.com>
2026-04-08 13:52:35 +0700

    Remove base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/flashlearn/", // Tên này phải trùng chính xác với tên Repository bạn tạo trên GitHub
+  base: "/", // Tên này phải trùng chính xác với tên Repository bạn tạo trên GitHub
 });
